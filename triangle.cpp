8f180f4e7b1e5ce2160469985a0db15d1bbf3864
Michael I <mivanit@hotmail.com>
2017-07-13 14:03:11 -0400

    begin implement corner tri checking

--- triangle.cpp
+++ triangle.cpp
@@ -156,29 +156,85 @@ public:
 		//check right
 		bool test_R = chk_adj(r,p,0,1,-1,0);
 
-		if (test_U && test_L && test_R)
+		//test if value should be changed
+		if (test_U || test_L || test_R)
 		{
-			changed = true;
-		}
-
-		//change actual value of card
-		if (changed)
-		{
+			//change actual value of card
 			cards[index].det = true;
+
+			return true;
 		}
 
-		return changed;
+		//if not changed, return false
+		return false;
 	}
 
-	//for checking 2 adjacent cards
+	//checking for triangles of size 2
+	bool chk_2tri(int index)
+	{
+
+	}
+
+	//checking for triangle corners of (3^x)+1 tris inside
+
+	//main function
+	bool chk_corners(int index)
+	{
+		bool loop_continue;
+		int i = 1;
+
+		while (loop_continue)
+		{
+			//continue until there are no 3^x + 1 tris remain inside
+			if ( ((3^i) + 1) < n )
+			{
+				//call corner checking function
+				chk_corner_tri( index, ((3^i) + 1) );
+
+				//iterate counter
+				i++;
+			}
+			else
+			{
+				loop_continue = false;
+			}
+		}
+	}
+
+	//checks for the x-th (3^x)+1 tri at an index
+	bool chk_corner_tri(int index, int x)
+	{
+		//get r/p of index
+		int r = trind[index].first;
+		int p = trind[index].second;
+
+		//triangle of size x => corners are x-1 away
+		int k = x - 1;
+
+		//check above
+		bool test_U = chk_adj(r,p,k,0,k,k);
+
+		//check left
+		bool test_L = chk_adj(r,p,0,-k,-k,-k);
+
+		//check right
+		bool test_R = chk_adj(r,p,0,k,-k,0);
+
+		return (test_U || test_L || test_R);
+	}
+
+
+	//for checking 2 cards by relative positioning
 	//returns true if both determined
 	//USAGE:
 	// takes original p/r, then modifiers
 	// original r, p, 1st r mod, p mod, 2nd r mod, p mod
 	bool chk_adj(int r, int p, int r1, int p1, int r2, int p2)
 	{
-		//first check for not at an edge
-		if ((0 < p+p1 <= r) && (0 < p+p2 <= r) && (r <= n) && (r > 1))
+		//TODO: throw error if base r/p outside
+
+		//first check everything is inside
+		if (chk_in(r+r1, p+p1) && chk_in(r+r1, p+p1))
 		{
 			//if not at the edge, check those two cards
 			bool test1 = cards[crd.to_index(r+r1,p+p1)].det;
@@ -188,9 +244,15 @@ public:
 			return (test1 && test2);
 		}
 
-		//youre still here? that means it was on an edge
+		//still here? that means it was on an edge
 		//just return false
 		return false;
+	}
+
+	//function for checking that a p/r are inside the triangle
+	bool chk_in(int r, int p)
+	{
+		return ((p > 0) && (p <= r) && (r <= n) && (r > 0));
 	}
 
 	void draw_tri()
